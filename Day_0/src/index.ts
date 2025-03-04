0c5196664da10b3a2c0cf72188e985db49fee2ba
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-04 09:37:07 +0545

    wrapping up ts

--- Day_0/src/index.ts
+++ Day_0/src/index.ts
@@ -21,3 +21,51 @@ interface User {
 }
 
 type Status = 'active' | 'inactive';
+
+// Generics
+
+// function wrapData<T>(data: T): { data: T } {
+//   return { data };
+// }
+
+// // Decorators!
+
+// function Log() {
+//   return function (target: any, key: string, descriptor: PropertyDescriptor) {
+//     const originalMethod = descriptor.value;
+//     descriptor.value = function (...args: any[]) {
+//       console.log(`Calling ${key} with args:`, args);
+//       return originalMethod.apply(this, args);
+//     };
+//   };
+// }
+
+// class UserService {
+//   @Log()
+//   getUser(id: number) {
+//     return { id, name: 'John Doe' };
+//   }
+// }
+
+// const service = new UserService();
+// service.getUser(1);
+// // Output: Calling getUser with args: [1]
+
+// Understanding promises
+
+async function fetchData(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('Data loaded'), 2000);
+  });
+}
+
+async function main() {
+  console.log('Start');
+  const data = await fetchData();
+  console.log(data);
+}
+
+main();
+// Output:
+// Start
+// (After 2 seconds) -> "Data loaded"
