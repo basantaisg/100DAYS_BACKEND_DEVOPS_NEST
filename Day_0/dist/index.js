889bd2d951f8b1f47041af0c5cbc47736abfa85c
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-04 09:27:11 +0545

    working with types and interfaces

--- Day_0/dist/index.js
+++ Day_0/dist/index.js
@@ -3,3 +3,9 @@ const greet = (name) => {
     return `Hello, ${name}`;
 };
 console.log(greet('Basanta'));
+// type annotation!
+let userName = 'Basanta';
+let age = 17;
+let isAdult = false;
+console.log(typeof userName, typeof age, typeof isAdult);
+// userName = 1; // error
--- Day_0/src/index.ts
+++ Day_0/src/index.ts
@@ -3,3 +3,21 @@ const greet = (name: string): string => {
 };
 
 console.log(greet('Basanta'));
+
+// type annotation!
+let userName: string = 'Basanta';
+let age: number = 17;
+let isAdult: boolean = false;
+
+console.log(typeof userName, typeof age, typeof isAdult);
+// userName = 1; // error
+
+// Working with interfaces and Types!
+
+interface User {
+  name: string;
+  age: number;
+  status: Status;
+}
+
+type Status = 'active' | 'inactive';
