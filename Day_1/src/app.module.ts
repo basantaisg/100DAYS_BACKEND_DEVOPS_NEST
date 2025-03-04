8df4dfdc3459542a648106dde5e72e28a1cf7306
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-04 11:10:14 +0545

    runnning

--- Day_1/src/app.module.ts
+++ Day_1/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo'; // Corrected import
 
 @Module({
   imports: [
+    // GraphQL setup using ApolloDriver
     GraphQLModule.forRoot({
-      autoSchemaFile: true,
+      driver: ApolloDriver, // Set Apollo as the GraphQL driver
+      autoSchemaFile: true, // Auto-generate the GraphQL schema
+      playground: true, // Enable GraphQL Playground for testing
     }),
   ],
 })
