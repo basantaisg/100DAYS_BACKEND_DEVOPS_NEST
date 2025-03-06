7b7b5604209f316734c95b68e60d03e70cb9ffa9
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-06 10:58:03 +0545

    adding configs

--- Day_3/src/app.module.ts
+++ Day_3/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot()], // this automatically loads env!
   controllers: [AppController],
   providers: [AppService],
 })
