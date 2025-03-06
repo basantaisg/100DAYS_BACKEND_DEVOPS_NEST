4fca1e1428c01008bc867a9b470bc9ab14d86e4c
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-06 10:42:56 +0545

    removing dist

--- Day_1/dist/users/users.resolver.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from './users.service';
-export declare class UsersResolver {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    getUsers(): {
-        id: number;
-        name: string;
-    }[];
-}
--- Day_1/dist/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class UsersService {
-    private users;
-    findAll(): {
-        id: number;
-        name: string;
-    }[];
-}
