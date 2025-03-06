460463ab763a8e516512947e518e71117d5f6e36
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-06 10:53:16 +0545

    using express shits

--- Day_3/src/middlewares/logger/logger.middleware.ts
+++ Day_3/src/middlewares/logger/logger.middleware.ts
@@ -1,8 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
   }
 }
