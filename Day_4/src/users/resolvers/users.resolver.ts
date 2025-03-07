8dbe82bee7668b58a66b8baccfbb0f0f8741fedd
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-07 18:58:06 +0545

    adding up resolver logic!

--- Day_4/src/users/resolvers/users.resolver.ts
+++ Day_4/src/users/resolvers/users.resolver.ts
@@ -1,5 +1,37 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from '../models/user.model';
+import { UsersService } from '../services/users.service';
+import { CreateUserInput, UpdateUserInput } from '../dto/user.dto';
 
 @Resolver(() => User)
-export class UsersResolver {}
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput) {
+    return this.usersService.createUser(data);
+  }
+
+  @Query(() => [User])
+  async getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @Query(() => User, { nullable: true })
+  async getUserById(@Args('íd') id: string) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserInput,
+  ) {
+    return this.usersService.updatedUser(id, data);
+  }
+
+  @Mutation(() => User)
+  async deleteUser(@Args('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+}
