4e557f3c95171d4ef038ab59e31262f21e013c95
=basantaisg <basantaparajuli.11@gmail.com>
2025-03-07 18:09:38 +0545

    Ola!

--- Day_3/src/users/users.module.ts
+++ Day_3/src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersResolver } from './resolvers/users.resolver';
 import { UsersService } from './services/users.service';
-import { UsersController } from './controllers/users.controller';
+// import { UsersController } from './controllers/users.controller';
 
 @Module({
   providers: [UsersResolver, UsersService],
   exports: [UsersService],
-  controllers: [UsersController],
 })
 export class UsersModule {}
